a5d72423f64393016cb4743f627cacb61c038656
ingenious3 <ishant.7123217@gmail.com>
2019-09-19 16:13:57 +0530

    version 1.0

---
+++ app/src/main/java/com/example/notekeeper/data/NoteKeeperData.kt
@@ -0,0 +1,13 @@
+package com.example.notekeeper.data
+
+data class CourseInfo(val courseId : String, val title : String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
+data class NoteInfo(var course : CourseInfo? = null, var title: String? = null, var text : String? = null) {
+    override fun toString(): String {
+        return title+"\n"+text
+    }
+}
